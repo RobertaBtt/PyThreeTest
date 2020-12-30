84728534a41cd874a3f8a5a98f49378a84662edf
= <=>
2020-12-30 15:55:13 +0100

    After the test is the implementation of function three and function one

--- functions/py_one.py
+++ functions/py_one.py
@@ -1,2 +1,21 @@
-def findFirstRepeated(integerVector1, integerVector2):
-    return None
+def findFirstRepeated(integer_vector1, integer_vector2):
+    size_vector1 = len(integer_vector1)
+    size_vector2 = len(integer_vector2)
+
+    # Just to do the smaller for-loop
+    if size_vector1 < size_vector2:
+        return compare_elements(integer_vector1,integer_vector2 )
+
+    # The else contains also the case: sizes of the vectors are the same
+    else:
+        return compare_elements(integer_vector2, integer_vector1)
+
+
+def compare_elements(vector_1, vector_2):
+    for element in vector_1:
+        if element in vector_2:
+            return element
+
+
+if __name__ == '__main__':
+    findFirstRepeated([],[])
--- functions/py_three.py
+++ functions/py_three.py
@@ -1,2 +1,57 @@
-def countChangeCoinInterval():
-    pass
+from bitarray import bitarray
+
+
+def countChangeCoinInterval(coins: str):
+    if coins !='':
+        try:
+            a = bitarray(coins)
+            mask2 = buildMaskReguralIntervals(len(a), True)
+            mask1 = buildMaskReguralIntervals(len(a), False)
+            count_pattern1 = 0
+            count_pattern2 = 0
+
+            for key, value in enumerate(a):
+                if value != mask1[key]:
+                    count_pattern1 += 1
+
+                if value != mask2[key]:
+                    count_pattern2 += 1
+
+            return min(count_pattern1, count_pattern2)
+        except ValueError:
+            pass
+    return None
+
+
+def buildMaskReguralIntervals(size: int, start_with: bool) -> bitarray:
+    mask = bitarray(size)
+    mask.setall(not start_with)
+    mask[1:size:2] = start_with
+    return mask
+
+
+if __name__ == '__main__':
+    #coins = '0110'
+    #print(countChangeCoinInterval(coins))
+
+    #coins = '10010000'
+    #print(countChangeCoinInterval(coins))
+
+#################################33
+    # coins = '01101011'  #Mask bitarray('10101010') return 3
+    # print(countChangeCoinInterval(coins))
+    #
+    # coins = '10010100' # Mask bitarray('10101010') return 5
+    # print(countChangeCoinInterval(coins))
+
+    #coins = '01101011'  #Mask bitarray('01010101') return 5
+    #print(countChangeCoinInterval(coins))
+
+    #coins = '10010100' # Mask bitarray('01010101') return 3
+    #print(countChangeCoinInterval(coins))
+
+    #coins = '0110'
+    #print(countChangeCoinInterval(coins))
+
+    coins = ''
+    print(countChangeCoinInterval(coins))
